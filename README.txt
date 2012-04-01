a73478a7edd96d2be68acd49160028afebd46993
CLechenne <Christophe.Lechenne@gmail.com>
2012-04-01 13:39:19 +0200

    v0.2

--- README.txt
+++ README.txt
@@ -5,4 +5,6 @@ Add EventLog(Turn 002 6-26-1941 Ax).txt
 Launch it 
 An export.csv is created in the same folder
 
+- version 0.2 : Add turn losses and cumulatives losses
+
 - version 0.1 : csv export fix
--- src/Load.java
+++ src/Load.java
@@ -14,22 +14,35 @@ public class Load {
 	 */
 	public static void main(String[] args) throws IOException {
 		
-		System.out.println("WITETracker v0.1");
+		System.out.println("WITETracker v0.2");
+		
+		// new export data
+		int axisCumulativeLosses = 0;
+		int axisCurrentLosses = 0;
+		int sovCumulativeLosses = 0;
+		int sovCurrentLosses = 0;
 		
 		File  folder = new File ("./");
 		File [] listOfFiles = folder.listFiles();
 
 		FileWriter fw = new FileWriter("./export.csv");
 		fw.write("Num;Date;" +
-				"Axis Combat;Axis Retreat;Axis Attrition;Axis Surrender;Axis Misc.;" + 
-				"Soviet Combat;Soviet Retreat;Soviet Attrition;Soviet Surrender;Soviet Misc.;"
+				"Axis Combat;Axis Retreat;Axis Attrition;Axis Surrender;Axis Misc.;Axis turn losses; Axis cum losses" + 
+				"Soviet Combat;Soviet Retreat;Soviet Attrition;Soviet Surrender;Soviet Misc.;Soviet turn losses; Soviet cum losses"
 				+ CAR_RET);
 		for (File f : listOfFiles) {
 			if (f.getName().indexOf("EventLog(") != -1) {
+				
 				System.out.println("File : " + f.getName());
 				
 				WITEParser wp = new WITEParser(f.getName());
 				Turn turn = wp.parse();		
+				
+				axisCurrentLosses = turn.axisCombat +  turn.axisRetreat + turn.axisAttrition + turn.axisSurrender + turn.axisMisc;
+				axisCumulativeLosses += axisCurrentLosses;
+				
+				sovCurrentLosses = turn.sovietCombat +  turn.sovietRetreat + turn.sovietAttrition + turn.sovietSurrender + turn.sovietMisc;
+				sovCumulativeLosses +=  sovCurrentLosses;
 				
 				try {
 					fw.write(turn.num + ";" 
@@ -39,11 +52,15 @@ public class Load {
 							+ turn.axisAttrition + ";"
 							+ turn.axisSurrender + ";"
 							+ turn.axisMisc + ";"
+							+ axisCurrentLosses + ";"
+							+ axisCumulativeLosses + ";"
 							+ turn.sovietCombat + ";"
 							+ turn.sovietRetreat + ";"
 							+ turn.sovietAttrition + ";"
 							+ turn.sovietSurrender + ";"
 							+ turn.sovietMisc + ";"
+							+ sovCurrentLosses + ";"
+							+ sovCumulativeLosses + ";"
 
 							+ CAR_RET);
 					
